e6bbfe0a4b4d56361c98bb9318b6610c78952035
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-10-15 14:18:16 +0200

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deploy React App</h1>
+      <h1>New title for web app!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
