ae80703741229f944c5be193291d92fdc6250496
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-12-09 13:46:10 +0100

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import examplePng from './assets/AlgorithmicGovernance.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>New title for web app!</h1>
+      <img src={examplePng} alt="" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
